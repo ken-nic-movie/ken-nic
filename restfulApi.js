e1197d20f1d0cd36add0a51629d8704e4338bc3c
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-07-26 09:55:21 -0500

    jquery added

---
+++ restfulApi.js
@@ -0,0 +1,42 @@
+const serverURL = "https://alluring-thunder-archaeology.glitch.me/movies";
+//
+// fetch(serverURL)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//
+//
+
+const reviewObj = {
+    restaurant_id: 1,
+    name: 'Codey',
+    rating: 5,
+    comments: "This is a really good place for coding and eating"
+};
+const url = 'https://codeup-restful-example.glitch.me/reviews';
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(reviewObj),
+};
+fetch(serverURL, options)
+    .then( response => console.log(response) ) /* review was created successfully */
+    .catch( error => console.error(error) ); /* handle errors */
+
+function AJAX(url, method = "GET", data) {
+    const options = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+    return fetch(url, options)
+        .then(res => res.json())
+        .then(responseData => responseData)
+        .catch(err => err)
+}
+
+AJAX(serverURL + "/3")
+    .then(data => console.log(data));
