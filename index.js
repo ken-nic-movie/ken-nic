2fc6bc3b5ec3db0fce4f4d9923e3bc3087a1281d
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-07-28 09:56:28 -0500

    added edit and submit

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ function fetchData() {
         console.log(error);
     })
 }
+
 //
 // function deleteCard() {
 //     let d = document.getElementById('full-card');
@@ -69,30 +70,6 @@ function fetchData() {
 
 
 
-// ul.addEventListener('click', (event) => {
-//     if(event.target.tagName === 'BUTTON') {
-//         const button = event.target;
-//         const li = button.parentNode;
-//         const ul = li.parentNode;
-//         if(button.textContent === 'remove') {
-//             ul.removeChild(li);
-//         } else if(button.textContent === 'edit') {
-//             const span = li.firstElementChild;
-//             const input = document.createElement('input');
-//             input.type = 'text';
-//             input.value = span.textContent;
-//             li.insertBefore(input, span);
-//             li.removeChild(span);
-//             button.textContent = 'save';
-//         } else if(button.textContent === 'save') {
-//             const input = li.firstElementChild;
-//             const span = document.createElement('span');
-//             span.textContent = input.value;
-//             li.insertBefore(span, input);
-//             li.removeChild(input);
-//             button.textContent = 'edit';
-//         }
-//     }
-// });
+
 
 fetchData();
